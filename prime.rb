375792dff25e59d5e3383fa1505624dfa0af3f5c
Kyle Vigorito <kyle.vigorito@gmail.com>
2017-08-01 23:53:15 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(number)
+ counter = 2
+ if number <=1
+   return false
+ end
+ if number == 2
+   return true
+ end
+  while counter < number
+    if number % counter == 0
+      return false
+    else
+      counter += 1
+    end
+  end
+  return true
+end
